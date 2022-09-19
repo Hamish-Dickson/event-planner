a19a7df5f95ca007ce3d4041e388bbc1af202445
Hamish <Hamishdickson98@gmail.com>
2022-09-19 20:12:26 +0100

    Created framework database .js files that we can populate with code 	modified:   index.html 	new file:   scripts/dbaction.js 	new file:   scripts/dbconnectionmanager.js

---
+++ scripts/dbaction.js
@@ -0,0 +1,20 @@
+/*todo: make the database functionality come from here*/
+function createEvent(){
+    //create an event. params will be defined as things that we need to store about the event.
+}
+
+function deleteEvent(id){
+    //simply delete the event
+}
+
+function updateEvent(){
+    //will need params for this but some way to pass a name as a param for the thing you want to update??? title, description, etc.?
+}
+
+function getEvent(id){
+    //simply return the event
+}
+
+function postToEvent(id, message){
+    //todo: post messages to the event page
+}
---
+++ scripts/dbconnectionmanager.js
@@ -0,0 +1,5 @@
+/*todo: provide db connectivity through here??? idk how js works maybe we need it in the dbaction.js*/
+
+function connection(){
+    //provide and return a db connection
+}
